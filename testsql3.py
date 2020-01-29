a13aa5a48c9c4d18660274d2256cfb99a4d7a391
NAYO OGILVIE <nathaniel.ogilvie@ibm.com>
2020-01-29 09:06:29 -0500

    Testing again

---
+++ testsql3.py
@@ -0,0 +1,11 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect('test.db')
+
+#information on openning csv found here
+#https://stackoverflow.com/questions/2887878/importing-a-csv-file-into-a-sqlite3-database-table-using-python
+
+
+
+print ("Opened database successfully");
