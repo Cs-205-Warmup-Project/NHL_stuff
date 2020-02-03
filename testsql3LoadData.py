9dd6c62d4cd44b927b64feba3aecf30cee47d25d
NAYO OGILVIE <nathaniel.ogilvie@ibm.com>
2020-02-03 08:30:05 -0500

    Changed loaddata to take in all data fro some reason it needs to iterate over all dictionary items?

--- testsql3LoadData.py
+++ testsql3LoadData.py
@@ -11,8 +11,8 @@ print ("Opened database successfully");
 
 cursor = conn.cursor()
 cursor.execute("CREATE TABLE game_goalie_stats (player_id, team_id, time_on_ice, assists, goals, shots, saves);")
-#cursor.execute("CREATE TABLE game_skater_stats (player_id, team_id, time_on_ice, assists, goals, shots, hits);")
-#cursor.execute("CREATE TABLE player_info (player_id, firstName, lastName, primaryPosition);")
+cursor.execute("CREATE TABLE game_skater_stats (player_id, team_id, time_on_ice, assists, goals, shots, hits);")
+cursor.execute("CREATE TABLE player_info (player_id, firstName, lastName, primaryPosition);")
 
 #iterate through csv file add intormation to dictionary then to column datastructure
 with open('game_goalie_stats.csv', newline='') as goalie_stats_csv:
@@ -21,19 +21,23 @@ with open('game_goalie_stats.csv', newline='') as goalie_stats_csv:
 
 #Below code gives an error, working it it currently
 
-#with open('GameSkaterStats.csv', newline ='') as game_skater_csv:
- #   game_skater_dict = csv.DictReader(game_skater_csv)
-  #  skater_stats_colm = [(i['player_id'], i['team_id'], i['time_on_ice'], i['assists'], i['goals'], i['shots'], i['hits']) for i in game_skater_dict]
+with open('GameSkaterStats.csv', newline ='') as game_skater_csv:
+    game_skater_dict = csv.DictReader(game_skater_csv)
+    for i in game_skater_dict:
+        print(i)
+    skater_stats_colm = [(i['player_id'], i['team_id'], i['time_on_ice'], i['assists'], i['goals'], i['shots'], i['hits']) for i in game_skater_dict]
 
-#with open('PlayerInfo1.csv', newline = '') as player_info_csv:
- #   player_info_dict = csv.DictReader(player_info_csv)
-  #  player_info_colm = [(i['player_id'], i['firstName'], i['lastName'], i['primaryPosition']) for i in player_info_dict]
+with open('PlayerInfo1.csv', newline = '') as player_info_csv:
+    player_info_dict = csv.DictReader(player_info_csv)
+    for i in player_info_dict:
+        print(i)
+    player_info_colm = [(i['player_id'], i['firstName'], i['lastName'], i['primaryPosition']) for i in player_info_dict]
 
 
 #add column information to the sqlite3 table
 cursor.executemany("INSERT INTO game_goalie_stats ( player_id, team_id, time_on_ice, assists, goals, shots, saves ) VALUES (?, ?, ?, ?, ?, ?, ?);", goalie_stats_colm)
-#cursor.executemany("INSERT INTO game_skater_stats ( player_id, team_id, time_on_ice, assists, goals, shots, hits ) VALUES (?, ?, ?, ?, ?, ?, ?);", skater_stats_colm)
-#cursor.executemany("INSERT INTO player_info ( player_id, firstName, lastName, primaryPosition ) VALUES (?, ?, ?, ?);", player_info_colm)
+cursor.executemany("INSERT INTO game_skater_stats ( player_id, team_id, time_on_ice, assists, goals, shots, hits ) VALUES (?, ?, ?, ?, ?, ?, ?);", skater_stats_colm)
+cursor.executemany("INSERT INTO player_info ( player_id, firstName, lastName, primaryPosition ) VALUES (?, ?, ?, ?);", player_info_colm)
 
 conn.commit()
 conn.close()
