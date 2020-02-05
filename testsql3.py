0d4b7c13d7da28b1f516077af236d1948731d99b
NAYO OGILVIE <nathaniel.ogilvie@ibm.com>
2020-02-05 18:43:44 -0500

    adding fuctionality blah

--- testsql3.py
+++ testsql3.py
@@ -11,16 +11,23 @@ print ("Opened database successfully");
 
 cursor = conn.cursor()
 #testing query of data
-cursor.execute("SELECT * FROM game_goalie_stats ORDER BY time_on_ice ;")
+#cursor.execute("SELECT * FROM game_goalie_stats ORDER BY time_on_ice ;")
+#print(cursor.fetchall())
+cursor.execute("SELECT * FROM game_goalie_stats;")
 print(cursor.fetchall())
 
 #Create fucntion to take in varables and spit out data
 def retrieveDataFirstLast(firstName, lastName, keyWord):
     #for test purpose
-    value = 31
-    cursor.execute("SELECT player_id FROM game_goalie_stats WHERE " + keyWord + "=?;",(value,))
+    #value = 31
+    #cursor.execute("SELECT player_id FROM game_goalie_stats WHERE " + keyWord + "=?;",(value,))
+    #search for player id
+    print(lastName)
+    #cursor.execute("SELECT * FROM player_info WHERE lastName=?;", (lastName,))
+    cursor.execute("SELECT * FROM player_info;")
+    #search for keyWord data based on player_id
     print(cursor.fetchall())
-    
-retrieveDataFirstLast("test2", "test1", "saves")
+
+retrieveDataFirstLast("test2", "Timonen", "saves")
 conn.commit()
 conn.close()
